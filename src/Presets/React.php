e35577144b9e69fe05fa3ed8d1957be072da6138
Kamal <kamal@iseekplant.com.au>
2021-06-16 06:26:32 +1000

    Update dependency, jest package added

--- src/Presets/React.php
+++ src/Presets/React.php
@@ -2,6 +2,8 @@
 
 namespace UsrNotFound\Ui\Presets;
 
+use UsrNotFound\Ui\Package;
+
 class React extends Preset
 {
     public static function install()
@@ -10,17 +12,14 @@ class React extends Preset
         static::updatePackages();
         static::updatePackages(false);
         static::updateWebpackConfiguration();
-        static::updateBootstrapping();
-        static::updateComponent();
         static::removeNodeModules();
         static::updateTailwindConfig();
-        static::updateScss();
         static::updateBableRc();
     }
 
     protected static function updatePackageArray(array $packages, $configurationKey)
     {
-        return array_merge(self::packages()[$configurationKey], $packages);
+        return array_merge(self::dependencies()[$configurationKey], $packages);
     }
 
     protected static function updateWebpackConfiguration()
@@ -28,24 +27,10 @@ class React extends Preset
         copy(__DIR__ . '/stubs/webpack.mix.js', base_path('webpack.mix.js'));
     }
 
-    protected static function updateComponent()
-    {
-        copy(__DIR__ . '/stubs/Example.js', resource_path('js/components/Example.js'));
-    }
-
-    protected static function updateBootstrapping()
-    {
-        copy(__DIR__ . '/stubs/app.js', resource_path('js/app.js'));
-    }
 
     protected static function updateTailwindConfig()
     {
         copy(__DIR__ . '/stubs/tailwind.config.js', base_path('tailwind.config.js'));
-    }
-
-    protected static function updateScss()
-    {
-        copy(__DIR__ . '/stubs/app.scss', resource_path('sass/app.scss'));
     }
 
     protected static function updateBableRc()
@@ -53,22 +38,11 @@ class React extends Preset
         copy(__DIR__ . '/stubs/.babelrc', base_path('.babelrc'));
     }
 
-    public static function packages()
+    public static function dependencies()
     {
         return [
-            'devDependencies' => [
-                '@babel/plugin-proposal-optional-chaining' => '^7.9.0',
-                '@babel/plugin-syntax-dynamic-import' => '^7.8.3',
-                '@babel/preset-react' => '^7.0.0',
-                'react' => '^16.2.0',
-                'react-dom' => '^16.2.0',
-                'cross-env' => '^7.0',
-                'tailwindcss' => '^1.4',
-            ],
-            'dependencies' => [
-                '@inertiajs/inertia' => '^0.2.1',
-                '@inertiajs/inertia-react' => '^0.2.0'
-            ]
+            'devDependencies' => Package::NPM_DEV_DEPENDENCY,
+            'dependencies' => Package::NPM_DEPENDENCY
         ];
     }
 }
